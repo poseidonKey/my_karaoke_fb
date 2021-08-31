7b2c204500401b68c1f8e36994d5c30d68a49f67
jsy <jsy1030@chollian.net>
2021-08-31 18:55:47 +0900

    4th -signIn, signUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
- 
+import 'package:firebase_auth/firebase_auth.dart' as firebaseAuth;
+import 'package:my_karaoke_fb/pages/home_page.dart';
+import 'package:my_karaoke_fb/pages/signin_page.dart';
+import 'package:my_karaoke_fb/pages/signup_page.dart';
+import 'package:my_karaoke_fb/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
-} 
- 
+}
+
 class MyApp extends StatelessWidget {
+  Widget isAuthenticated(BuildContext context) {
+    if (context.watch<firebaseAuth.User>() != null) {
+      return HomePage();
+    }
+    return SigninPage();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+    return MultiProvider(
+      providers: [
+        StreamProvider<firebaseAuth.User>.value(
+          value: firebaseAuth.FirebaseAuth.instance.authStateChanges(),
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
+        ChangeNotifierProvider<AuthProvider>(
+          create: (context) => AuthProvider(),
         ),
+      ],
+      child: MaterialApp(
+        title: 'Song_jsy',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Builder(
+          builder: (context) => isAuthenticated(context),
+        ),
+        routes: {
+          SigninPage.routeName: (context) => SigninPage(),
+          SignupPage.routeName: (context) => SignupPage(),
+          // NotesPage.routeName: (context) => NotesPage(),
+        },
       ),
     );
   }
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key key }) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Widget> _pages = [
+    // NotesPage(),
+    // ProfilePage(),
+  ];
+  int _selectedIndex = 0; 
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
